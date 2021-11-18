d3a4ac250f54aab0f9ec7bab9586deaa249a803b
sachin <sachin.kumar@quovantis.com>
2021-11-18 21:31:23 +0530

    user test cases

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/user");
 
 module.exports = () => {
   router.get("/", userController.getUsers);
-  router.post("/", userController.createUser);
+  // router.post("/", userController.createUser);
   router.get("/:id", userController.getUser);
   router.put("/:id", userController.updateUser);
   router.delete("/:id", userController.deleteUser);
